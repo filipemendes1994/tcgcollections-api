bb4491967d8bc72c73d15ac5fa17eef24966400d
filipemendes <filipe.mendes@wit-software.com>
2021-01-31 23:51:56 +0000

    feat: add ORM

--- config/database.js
+++ config/database.js
@@ -1,14 +1,9 @@
 const mysql = require('mysql');
+const config = require('config');
+const { Sequelize } = require('sequelize');
 
 module.exports = () => {
-  const connection = mysql.createConnection({
-    host: '172.17.0.1',
-    port: '3306',
-    user: 'user',
-    password: 'password',
-    database: 'tcgcollections',
-  });
-
-  connection.connect();
-  return connection;
+  const connectionUri =
+    `mysql://${config.get('server.dbUser')}:${config.get('server.dbPassword')}@${config.get('server.dbHost')}:${config.get('server.dbPort')}/${config.get('server.dbDatabase')}`;
+  return new Sequelize(connectionUri, { define: { timestamps: false } });
 }
